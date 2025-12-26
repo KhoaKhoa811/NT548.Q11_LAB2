3092057901f83bab804c37b34153b80bc2091616
willingWill17 <nguyentienthang258@gmail.com>
2025-12-26 22:56:43 +0700

    Commit7

--- exercise_1/terraform.tf
+++ exercise_1/terraform.tf
@@ -36,7 +36,7 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "vprofileactions0811"
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = "vprofileactions0811"
+#   force_destroy = true
+# }
